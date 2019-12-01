2fca303985cc524588f9d8fb7d1932cae90952c0
trebinor <trebinor@users.noreply.github.com>
2019-11-30 23:17:52 -0500

    Added day1 part2 from 2018 as placeholder.

--- src/day01.rs
+++ src/day01.rs
@@ -5,3 +5,11 @@ pub fn solve_1a(input: &str) -> i32 {
         .map(|l| l.trim().parse::<i32>().unwrap())
         .sum()
 }
+
+#[aoc(day01, part2)]
+pub fn solve_1b(input: &str) -> i32 {
+    input
+        .lines()
+        .map(|l| l.trim().parse::<i32>().unwrap())
+        .sum()
+}
