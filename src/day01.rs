b2fc294c76b2eb9273b782e2bd1e9ff66bdf481e
trebinor <trebinor@users.noreply.github.com>
2019-12-01 00:19:48 -0500

    Quick and dirty solutions for day1.

--- src/day01.rs
+++ src/day01.rs
@@ -1,15 +1,28 @@
+use std::cmp;
+
 #[aoc(day01, part1)]
 pub fn solve_1a(input: &str) -> i32 {
     input
         .lines()
-        .map(|l| l.trim().parse::<i32>().unwrap())
+        .map(|l| l.trim().parse::<f32>().unwrap())
+        .map(|f| ((f / 3.0) as i32) - 2)
         .sum()
 }
 
 #[aoc(day01, part2)]
 pub fn solve_1b(input: &str) -> i32 {
-    input
-        .lines()
-        .map(|l| l.trim().parse::<i32>().unwrap())
-        .sum()
+    let modules = input.lines().map(|l| l.trim().parse::<i32>().unwrap());
+    let mut fuel: i32 = 0;
+    for fuels in modules {
+        let mut module_fuel = fuel_mass(fuels);
+        while module_fuel != 0 {
+            fuel += module_fuel;
+            module_fuel = fuel_mass(module_fuel);
+        }
+    }
+    fuel
+}
+
+fn fuel_mass(module: i32) -> i32 {
+    cmp::max(0, ((module / 3.0 as i32) - 2))
 }
