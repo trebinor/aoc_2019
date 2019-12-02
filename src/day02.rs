216b3fb93440e0e5da423077fe93b54a9a4d4b56
trebinor <trebinor@users.noreply.github.com>
2019-12-02 01:26:43 -0500

    Cleanup.

--- src/day02.rs
+++ src/day02.rs
@@ -1,5 +1,3 @@
-//use std::cmp;
-
 #[aoc(day02, part1, original)]
 pub fn original_2a(input: &str) -> u32 {
     let mut v: Vec<u32> = input
@@ -26,16 +24,16 @@ fn compute(mut v: &mut Vec<u32>) -> u32 {
 }
 
 fn do_opcode_1(v: &mut Vec<u32>, i: usize) {
-    let oi1: usize = v[i + 1] as usize;
-    let oi2: usize = v[i + 2] as usize;
-    let loc: usize = v[i + 3] as usize;
-    v[loc] = v[oi1] + v[oi2];
+    let ia1: usize = v[i + 1] as usize;
+    let ia2: usize = v[i + 2] as usize;
+    let oa: usize = v[i + 3] as usize;
+    v[oa] = v[ia1] + v[ia2];
 }
 fn do_opcode_2(v: &mut Vec<u32>, i: usize) {
-    let oi1: usize = v[i + 1] as usize;
-    let oi2: usize = v[i + 2] as usize;
-    let loc: usize = v[i + 3] as usize;
-    v[loc] = v[oi1] * v[oi2];
+    let ia1: usize = v[i + 1] as usize;
+    let ia2: usize = v[i + 2] as usize;
+    let oa: usize = v[i + 3] as usize;
+    v[oa] = v[ia1] * v[ia2];
 }
 
 const PART2_EXPECTED_OUTPUT: u32 = 19690720;
@@ -57,73 +55,3 @@ pub fn original_1b(input: &str) -> u32 {
     }
     panic!()
 }
-/*
-#[aoc(day01, part2, original)]
-pub fn original_1b(input: &str) -> i32 {
-    let modules = input.lines().map(|l| l.trim().parse::<i32>().unwrap());
-    let mut fuel: i32 = 0;
-    for fuels in modules {
-        let mut module_fuel = original_fuel_mass_compound(fuels);
-        while module_fuel != 0 {
-            fuel += module_fuel;
-            module_fuel = original_fuel_mass_compound(module_fuel);
-        }
-    }
-    fuel
-}
-
-fn revised_fuel_mass_simple(module: u32) -> u32 {
-    (module / 3.0 as u32) - 2
-}
-
-fn original_fuel_mass_compound(module: i32) -> i32 {
-    cmp::max(0, module / 3.0 as i32 - 2)
-}
-
-fn revised_fuel_mass_compound(mass: u32) -> u32 {
-    match mass {
-        0 => 0,
-        _ => mass + revised_fuel_mass_compound(cmp::max(0, (mass as i32 / 3.0 as i32) - 2) as u32),
-    }
-}
-
-#[aoc(day01, part1, revised)]
-pub fn revised_1a(input: &str) -> u32 {
-    input
-        .lines()
-        .map(|l| revised_fuel_mass_simple(l.trim().parse::<u32>().unwrap()))
-        .sum()
-}
-
-#[aoc(day01, part2, revised)]
-pub fn revised_1b(input: &str) -> u32 {
-    input
-        .lines()
-        .map(|l| {
-            let mass = l.trim().parse::<u32>().unwrap();
-            revised_fuel_mass_compound(mass) - mass
-        })
-        .sum()
-}
-
-#[cfg(test)]
-mod tests {
-    use day01::original_1a;
-    use day01::original_1b;
-    use std::fs;
-    const ANSWER_1A: i32 = 3369286;
-    const ANSWER_1B: i32 = 5051054;
-
-    #[test]
-    fn original() {
-        assert_eq!(
-            ANSWER_1A,
-            original_1a(&fs::read_to_string("input/2019/day1.txt").unwrap())
-        );
-        assert_eq!(
-            ANSWER_1B,
-            original_1b(&fs::read_to_string("input/2019/day1.txt").unwrap())
-        );
-    }
-}
-*/
