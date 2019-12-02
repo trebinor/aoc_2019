6581752644dbfd38261775a34c2737e115f3e386
trebinor <trebinor@users.noreply.github.com>
2019-12-02 01:01:06 -0500

    Added day2 input and day2 part1 working.

--- src/day02.rs
+++ src/day02.rs
@@ -1,8 +1,25 @@
 //use std::cmp;
 
 #[aoc(day02, part1, original)]
-pub fn original_2a(input: &str) -> i32 {
-
+pub fn original_2a(input: &str) -> u32 {
+    let mut v: Vec<u32> = input
+        .split(',')
+        .map(|n| n.parse::<u32>().unwrap())
+        .collect();
+    v[1] = 12;
+    v[2] = 2;
+    let mut i: usize = 0;
+    loop {
+        println!("{:?}", v);
+        match v[i] {
+            1 => do_opcode_1(&mut v, i),
+            2 => do_opcode_2(&mut v, i),
+            99 => break,
+            _ => panic!(),
+        }
+        i += 4;
+    }
+    v[0]
     /*
     input
         .lines()
@@ -10,6 +27,19 @@ pub fn original_2a(input: &str) -> i32 {
         .map(|f| ((f / 3.0) as i32) - 2)
         .sum()
     */
+}
+
+fn do_opcode_1(v: &mut Vec<u32>, i: usize) {
+    let oi1: usize = v[i + 1] as usize;
+    let oi2: usize = v[i + 2] as usize;
+    let loc: usize = v[i + 3] as usize;
+    v[loc] = v[oi1] + v[oi2];
+}
+fn do_opcode_2(v: &mut Vec<u32>, i: usize) {
+    let oi1: usize = v[i + 1] as usize;
+    let oi2: usize = v[i + 2] as usize;
+    let loc: usize = v[i + 3] as usize;
+    v[loc] = v[oi1] * v[oi2];
 }
 
 /*
