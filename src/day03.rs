44a264e1a7ed710ea2a78fe070b3702b89bfb549
trebinor <trebinor@users.noreply.github.com>
2019-12-03 14:25:57 -0500

    Working day3 part1.

--- src/day03.rs
+++ src/day03.rs
@@ -11,12 +11,26 @@ pub fn original_3a(input: &str) -> u32 {
     let v: Vec<&str> = input.split('\n').collect();
     let v1 = v[0].trim().split(',').collect();
     let v2 = v[1].trim().split(',').collect();
-    let mut grid = vec![vec![0; GRID_X]; GRID_Y];
-    mut_grid(&mut grid, v1);
-    mut_grid(&mut grid, v2);
+    let mut grid1 = vec![vec![false; GRID_X]; GRID_Y];
+    let mut grid2 = vec![vec![false; GRID_X]; GRID_Y];
+    mut_grid(&mut grid1, v1);
+    mut_grid(&mut grid2, v2);
+
+    let mut manhattans = Vec::new();
+    for x in 0..GRID_X {
+        for y in 0..GRID_Y {
+            if grid1[x][y] && grid2[x][y] {
+                let manhattan = ((x as i32) - (GRID_X as i32) / 2).abs()
+                    + ((y as i32) - (GRID_Y as i32) / 2).abs();
+                println!("({},{}) {}", x, y, manhattan);
+                manhattans.push(manhattan);
+            }
+        }
+    }
+    *manhattans.iter().min().unwrap() as u32
 }
 
-fn mut_grid(grid: &mut Vec<Vec<u32>>, wires: Vec<&str>) {
+fn mut_grid(grid: &mut Vec<Vec<bool>>, wires: Vec<&str>) {
     let mut wire: Wire = Wire {
         x: GRID_X / 2,
         y: GRID_Y / 2,
@@ -26,22 +40,22 @@ fn mut_grid(grid: &mut Vec<Vec<u32>>, wires: Vec<&str>) {
         let dist = w.get(1..).unwrap().parse::<usize>().unwrap();
         if dir == 'R' {
             for d in 0..dist {
-                grid[wire.x + d][wire.y] += 1;
+                grid[wire.x + d][wire.y] = true;
             }
             wire.x += dist;
         } else if dir == 'U' {
             for d in 0..dist {
-                grid[wire.x][wire.y + d] += 1;
+                grid[wire.x][wire.y + d] = true;
             }
             wire.y += dist;
         } else if dir == 'L' {
             for d in 0..dist {
-                grid[wire.x - d][wire.y] += 1;
+                grid[wire.x - d][wire.y] = true;
             }
             wire.x -= dist;
         } else if dir == 'D' {
             for d in 0..dist {
-                grid[wire.x][wire.y - d] += 1;
+                grid[wire.x][wire.y - d] = true;
             }
             wire.y -= dist;
         }
@@ -49,9 +63,31 @@ fn mut_grid(grid: &mut Vec<Vec<u32>>, wires: Vec<&str>) {
 }
 
 /*
-const PART2_EXPECTED_OUTPUT: u32 = 19690720;
+//const PART2_EXPECTED_OUTPUT: u32 = 19690720;
 #[aoc(day03, part2, original)]
 pub fn original_3b(input: &str) -> u32 {
+    let v: Vec<&str> = input.split('\n').collect();
+    let v1 = v[0].trim().split(',').collect();
+    let v2 = v[1].trim().split(',').collect();
+    let mut grid1 = vec![vec![false; GRID_X]; GRID_Y];
+    let mut grid2 = vec![vec![false; GRID_X]; GRID_Y];
+    mut_grid(&mut grid1, v1);
+    mut_grid(&mut grid2, v2);
+
+    let mut manhattans = Vec::new();
+    for x in 0..GRID_X {
+        for y in 0..GRID_Y {
+            if grid1[x][y] && grid2[x][y] {
+                let manhattan = ((x as i32) - (GRID_X as i32) / 2).abs()
+                    + ((y as i32) - (GRID_Y as i32) / 2).abs();
+                println!("({},{}) {}", x, y, manhattan);
+                manhattans.push(manhattan);
+            }
+        }
+    }
+
+    // find shortest paths to each intersection and return minimum sum
+
     let v: Vec<u32> = input
         .split(',')
         .map(|n| n.parse::<u32>().unwrap())
@@ -69,12 +105,13 @@ pub fn original_3b(input: &str) -> u32 {
     panic!()
 }
 */
+
 #[cfg(test)]
 mod tests {
     use day03::original_3a;
     use day03::original_3b;
     use std::fs;
-    const ANSWER_3A: u32 = 5866714;
+    const ANSWER_3A: u32 = 557;
     const ANSWER_3B: u32 = 5208;
 
     #[test]
