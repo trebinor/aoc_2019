6516f650791b8a9544616da72777bc8d98444468
trebinor <trebinor@users.noreply.github.com>
2019-12-03 16:23:27 -0500

    Day3 part2 working.

--- src/day03.rs
+++ src/day03.rs
@@ -1,7 +1,7 @@
 const GRID_X: usize = 30000;
 const GRID_Y: usize = 30000;
 
-struct Wire {
+struct GridPoint {
     x: usize,
     y: usize,
 }
@@ -9,12 +9,12 @@ struct Wire {
 #[aoc(day03, part1, original)]
 pub fn original_3a(input: &str) -> u32 {
     let v: Vec<&str> = input.split('\n').collect();
-    let v1 = v[0].trim().split(',').collect();
-    let v2 = v[1].trim().split(',').collect();
+    let mut v1 = v[0].trim().split(',').collect();
+    let mut v2 = v[1].trim().split(',').collect();
     let mut grid1 = vec![vec![false; GRID_X]; GRID_Y];
     let mut grid2 = vec![vec![false; GRID_X]; GRID_Y];
-    mut_grid(&mut grid1, v1);
-    mut_grid(&mut grid2, v2);
+    mut_grid(&mut grid1, &mut v1);
+    mut_grid(&mut grid2, &mut v2);
 
     let mut manhattans = Vec::new();
     for x in 0..GRID_X {
@@ -22,16 +22,18 @@ pub fn original_3a(input: &str) -> u32 {
             if grid1[x][y] && grid2[x][y] {
                 let manhattan = ((x as i32) - (GRID_X as i32) / 2).abs()
                     + ((y as i32) - (GRID_Y as i32) / 2).abs();
-                println!("({},{}) {}", x, y, manhattan);
-                manhattans.push(manhattan);
+                if manhattan != 0 {
+                    println!("({},{}) {}", x, y, manhattan);
+                    manhattans.push(manhattan);
+                }
             }
         }
     }
     *manhattans.iter().min().unwrap() as u32
 }
 
-fn mut_grid(grid: &mut Vec<Vec<bool>>, wires: Vec<&str>) {
-    let mut wire: Wire = Wire {
+fn mut_grid(grid: &mut Vec<Vec<bool>>, wires: &mut Vec<&str>) {
+    let mut grid_point: GridPoint = GridPoint {
         x: GRID_X / 2,
         y: GRID_Y / 2,
     };
@@ -40,71 +42,111 @@ fn mut_grid(grid: &mut Vec<Vec<bool>>, wires: Vec<&str>) {
         let dist = w.get(1..).unwrap().parse::<usize>().unwrap();
         if dir == 'R' {
             for d in 0..dist {
-                grid[wire.x + d][wire.y] = true;
+                grid[grid_point.x + d][grid_point.y] = true;
             }
-            wire.x += dist;
+            grid_point.x += dist;
         } else if dir == 'U' {
             for d in 0..dist {
-                grid[wire.x][wire.y + d] = true;
+                grid[grid_point.x][grid_point.y + d] = true;
             }
-            wire.y += dist;
+            grid_point.y += dist;
         } else if dir == 'L' {
             for d in 0..dist {
-                grid[wire.x - d][wire.y] = true;
+                grid[grid_point.x - d][grid_point.y] = true;
             }
-            wire.x -= dist;
+            grid_point.x -= dist;
         } else if dir == 'D' {
             for d in 0..dist {
-                grid[wire.x][wire.y - d] = true;
+                grid[grid_point.x][grid_point.y - d] = true;
             }
-            wire.y -= dist;
+            grid_point.y -= dist;
         }
     }
 }
 
-/*
 //const PART2_EXPECTED_OUTPUT: u32 = 19690720;
 #[aoc(day03, part2, original)]
-pub fn original_3b(input: &str) -> u32 {
+pub fn original_3b(input: &str) -> usize {
     let v: Vec<&str> = input.split('\n').collect();
-    let v1 = v[0].trim().split(',').collect();
-    let v2 = v[1].trim().split(',').collect();
+    let mut v1 = v[0].trim().split(',').collect();
+    let mut v2 = v[1].trim().split(',').collect();
     let mut grid1 = vec![vec![false; GRID_X]; GRID_Y];
     let mut grid2 = vec![vec![false; GRID_X]; GRID_Y];
-    mut_grid(&mut grid1, v1);
-    mut_grid(&mut grid2, v2);
+    mut_grid(&mut grid1, &mut v1);
+    mut_grid(&mut grid2, &mut v2);
 
-    let mut manhattans = Vec::new();
+    let mut intersections = Vec::new();
     for x in 0..GRID_X {
         for y in 0..GRID_Y {
             if grid1[x][y] && grid2[x][y] {
-                let manhattan = ((x as i32) - (GRID_X as i32) / 2).abs()
-                    + ((y as i32) - (GRID_Y as i32) / 2).abs();
-                println!("({},{}) {}", x, y, manhattan);
-                manhattans.push(manhattan);
+                intersections.push(GridPoint { x: x, y: y });
             }
         }
     }
 
     // find shortest paths to each intersection and return minimum sum
-
-    let v: Vec<u32> = input
-        .split(',')
-        .map(|n| n.parse::<u32>().unwrap())
-        .collect();
-    for noun in 0..99 {
-        for verb in 0..99 {
-            let mut v_cloned = v.clone();
-            v_cloned[1] = noun;
-            v_cloned[2] = verb;
-            if compute(&mut v_cloned) == PART2_EXPECTED_OUTPUT {
-                return 100 * noun + verb;
+    intersections
+        .into_iter()
+        .map(|mut i| walk_wires(&mut i, &mut v1) + walk_wires(&mut i, &mut v2))
+        .min_by(|x, y| {
+            if *x == 0 {
+                std::cmp::Ordering::Greater
+            } else if *y == 0 {
+                std::cmp::Ordering::Less
+            } else {
+                x.cmp(y)
             }
+        })
+        .unwrap()
+}
+
+fn walk_wires(i: &mut GridPoint, wires: &mut Vec<&str>) -> usize {
+    let mut steps: usize = 0;
+    let mut x = GRID_X / 2;
+    let mut y = GRID_Y / 2;
+    'outer: for w in wires {
+        let dir = w.chars().next().unwrap();
+        let dist = w.get(1..).unwrap().parse::<usize>().unwrap();
+        if dir == 'R' {
+            for d in 0..dist {
+                if x + d == i.x && y == i.y {
+                    steps += d;
+                    break 'outer;
+                }
+            }
+            x += dist;
+            steps += dist;
+        } else if dir == 'U' {
+            for d in 0..dist {
+                if x == i.x && y + d == i.y {
+                    steps += d;
+                    break 'outer;
+                }
+            }
+            y += dist;
+            steps += dist;
+        } else if dir == 'L' {
+            for d in 0..dist {
+                if x - d == i.x && y == i.y {
+                    steps += d;
+                    break 'outer;
+                }
+            }
+            x -= dist;
+            steps += dist;
+        } else if dir == 'D' {
+            for d in 0..dist {
+                if x == i.x && y - d == i.y {
+                    steps += d;
+                    break 'outer;
+                }
+            }
+            y -= dist;
+            steps += dist;
         }
     }
-    panic!()
+    steps
 }
-*/
 
 #[cfg(test)]
 mod tests {
@@ -112,7 +154,7 @@ mod tests {
     use day03::original_3b;
     use std::fs;
     const ANSWER_3A: u32 = 557;
-    const ANSWER_3B: u32 = 5208;
+    const ANSWER_3B: u32 = 56410;
 
     #[test]
     fn original() {
