4884709e7cc8e600e9b3acf8d0e5e215b67d22de
trebinor <trebinor@users.noreply.github.com>
2019-12-12 17:47:27 -0500

    Supporting code for Day 12.

--- src/day12.rs
+++ src/day12.rs
@@ -1,10 +1,55 @@
 use regex::Regex;
 
-#[derive(Debug)]
+#[derive(Debug, Clone, Copy)]
 pub struct Position {
     x: i32,
     y: i32,
     z: i32,
+}
+
+#[derive(Debug, Clone)]
+pub struct Velocity {
+    x: i32,
+    y: i32,
+    z: i32,
+}
+
+impl Position {
+    pub fn new() -> Position {
+        Position { x: 0, y: 0, z: 0 }
+    }
+}
+
+impl Velocity {
+    pub fn new() -> Velocity {
+        Velocity { x: 0, y: 0, z: 0 }
+    }
+}
+
+#[derive(Clone)]
+pub struct Planet {
+    old_position: Position,
+    position: Position,
+    old_velocity: Velocity,
+    velocity: Velocity,
+}
+
+impl Planet {
+    pub fn new() -> Planet {
+        Planet {
+            old_position: Position::new(),
+            position: Position::new(),
+            old_velocity: Velocity::new(),
+            velocity: Velocity::new(),
+        }
+    }
+    pub fn potential_energy(&self) -> i32 {
+        self.position.x + self.position.y + self.position.z
+    }
+
+    pub fn kinetic_energy(&self) -> i32 {
+        self.velocity.x + self.velocity.y + self.velocity.z
+    }
 }
 
 #[aoc_generator(day12)]
@@ -25,8 +70,18 @@ pub fn generator(input: &str) -> Vec<Position> {
 }
 
 #[aoc(day12, part1)]
-pub fn total_energy(v: &[Position]) -> u32 {
-    0
+pub fn total_energy_all_planets(v: &[Position]) -> i32 {
+    let mut planets: Vec<Planet> = Vec::new();
+    for position in v {
+        let mut planet = Planet::new();
+        planet.position = *position;
+        planets.push(planet);
+    }
+    //TODO: apply gravity and velocity in loop
+    planets
+        .iter()
+        .map(|p| p.potential_energy() * p.kinetic_energy())
+        .sum()
 }
 
 #[aoc(day12, part2)]
