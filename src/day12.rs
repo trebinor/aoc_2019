a9bd8134cea6051fa0f0495181210ca29692df15
trebinor <trebinor@users.noreply.github.com>
2019-12-13 01:44:55 -0500

    Completed day 12 part 1, but part 2 definitely needs a better way to simulate 4 bodies.  This implementation gobbles up all the RAM.

--- src/day12.rs
+++ src/day12.rs
@@ -1,18 +1,35 @@
 use regex::Regex;
+use std::collections::HashSet;
 
-#[derive(Debug, Clone, Copy)]
+#[derive(Debug, Clone, Copy, Hash, Eq, PartialEq)]
 pub struct Position {
     x: i32,
     y: i32,
     z: i32,
 }
 
-#[derive(Debug, Clone)]
+/*
+impl PartialEq for Position {
+    fn eq(&self, other: &Position) -> bool {
+        self.x == other.x && self.y == other.y && self.z == other.z
+    }
+}
+*/
+
+#[derive(Debug, Clone, Copy, Hash, Eq, PartialEq)]
 pub struct Velocity {
     x: i32,
     y: i32,
     z: i32,
 }
+
+/*
+impl PartialEq for Velocity {
+    fn eq(&self, other: &Velocity) -> bool {
+        self.x == other.x && self.y == other.y && self.z == other.z
+    }
+}
+*/
 
 impl Position {
     pub fn new() -> Position {
@@ -26,29 +43,33 @@ impl Velocity {
     }
 }
 
-#[derive(Clone)]
-pub struct Planet {
-    old_position: Position,
+#[derive(Clone, Copy, Hash, Eq, PartialEq)]
+pub struct PlanetaryBody {
     position: Position,
-    old_velocity: Velocity,
     velocity: Velocity,
 }
 
-impl Planet {
-    pub fn new() -> Planet {
-        Planet {
-            old_position: Position::new(),
+/*
+impl PartialEq for PlanetaryBody {
+    fn eq(&self, other: &PlanetaryBody) -> bool {
+        self.position == other.position && self.velocity == other.velocity
+    }
+}
+*/
+
+impl PlanetaryBody {
+    pub fn new() -> PlanetaryBody {
+        PlanetaryBody {
             position: Position::new(),
-            old_velocity: Velocity::new(),
             velocity: Velocity::new(),
         }
     }
     pub fn potential_energy(&self) -> i32 {
-        self.position.x + self.position.y + self.position.z
+        self.position.x.abs() + self.position.y.abs() + self.position.z.abs()
     }
 
     pub fn kinetic_energy(&self) -> i32 {
-        self.velocity.x + self.velocity.y + self.velocity.z
+        self.velocity.x.abs() + self.velocity.y.abs() + self.velocity.z.abs()
     }
 }
 
@@ -69,22 +90,109 @@ pub fn generator(input: &str) -> Vec<Position> {
     positions
 }
 
+const ITERATIONS: u32 = 1000;
+
 #[aoc(day12, part1)]
-pub fn total_energy_all_planets(v: &[Position]) -> i32 {
-    let mut planets: Vec<Planet> = Vec::new();
+pub fn total_energy_all_planets(v: &[Position]) -> u64 {
+    let mut bodies: Vec<PlanetaryBody> = Vec::new();
     for position in v {
-        let mut planet = Planet::new();
-        planet.position = *position;
-        planets.push(planet);
+        let mut body = PlanetaryBody::new();
+        body.position = *position;
+        bodies.push(body);
     }
-    //TODO: apply gravity and velocity in loop
-    planets
+    for i in 1..=ITERATIONS {
+        //Is there a more rustic way to double-iterate over a vec?
+        for a in 0..bodies.len() {
+            for b in 0..bodies.len() {
+                if a == b {
+                    continue;
+                }
+                bodies[a].velocity.x += if bodies[b].position.x > bodies[a].position.x {
+                    1
+                } else if bodies[b].position.x < bodies[a].position.x {
+                    -1
+                } else {
+                    0
+                };
+                bodies[a].velocity.y += if bodies[b].position.y > bodies[a].position.y {
+                    1
+                } else if bodies[b].position.y < bodies[a].position.y {
+                    -1
+                } else {
+                    0
+                };
+                bodies[a].velocity.z += if bodies[b].position.z > bodies[a].position.z {
+                    1
+                } else if bodies[b].position.z < bodies[a].position.z {
+                    -1
+                } else {
+                    0
+                };
+            }
+        }
+        for a in 0..bodies.len() {
+            bodies[a].position.x += bodies[a].velocity.x;
+            bodies[a].position.y += bodies[a].velocity.y;
+            bodies[a].position.z += bodies[a].velocity.z;
+        }
+    }
+
+    bodies
         .iter()
-        .map(|p| p.potential_energy() * p.kinetic_energy())
+        .map(|p| p.potential_energy() as u64 * p.kinetic_energy() as u64)
         .sum()
 }
 
 #[aoc(day12, part2)]
-pub fn part_2(v: &[Position]) -> u32 {
-    0
+pub fn part_2(v: &[Position]) -> u64 {
+    let mut bodies: Vec<PlanetaryBody> = Vec::new();
+    for position in v {
+        let mut body = PlanetaryBody::new();
+        body.position = *position;
+        bodies.push(body);
+    }
+    let mut n_body_simulation: HashSet<Vec<PlanetaryBody>> = HashSet::new();
+    let mut i: u64 = 0;
+    loop {
+        //why do these not work here, but they do work in part 1
+        for a in 0..bodies.len() {
+            for b in 0..bodies.len() {
+                if a == b {
+                    continue;
+                }
+                bodies[a].velocity.x += if bodies[b].position.x > bodies[a].position.x {
+                    1
+                } else if bodies[b].position.x < bodies[a].position.x {
+                    -1
+                } else {
+                    0
+                };
+                bodies[a].velocity.y += if bodies[b].position.y > bodies[a].position.y {
+                    1
+                } else if bodies[b].position.y < bodies[a].position.y {
+                    -1
+                } else {
+                    0
+                };
+                bodies[a].velocity.z += if bodies[b].position.z > bodies[a].position.z {
+                    1
+                } else if bodies[b].position.z < bodies[a].position.z {
+                    -1
+                } else {
+                    0
+                };
+            }
+        }
+
+        for a in 0..bodies.len() {
+            bodies[a].position.x += bodies[a].velocity.x;
+            bodies[a].position.y += bodies[a].velocity.y;
+            bodies[a].position.z += bodies[a].velocity.z;
+        }
+        i += 1;
+        if !n_body_simulation.insert(bodies.clone()) {
+            break;
+        }
+    }
+    i
 }
