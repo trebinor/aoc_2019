9011172b78c1e4d9bd6f94d61d4e11a3bd6cd40e
trebinor <trebinor@users.noreply.github.com>
2019-12-31 01:14:39 -0500

    Day 24 part 2, but not sure why it doesn't work for the unit tests or solution.

--- src/day24.rs
+++ src/day24.rs
@@ -51,6 +51,7 @@ fn total_bugs(grid: GridLevel) -> usize {
     sum
 }
 
+#[allow(dead_code)]
 fn print_generation(grid: [[Life; GRID_X]; GRID_Y]) {
     for i in 0..GRID_X {
         for j in 0..GRID_Y {
@@ -73,32 +74,220 @@ fn new_generation(grid: &mut [[Life; GRID_X]; GRID_Y]) {
     }
     for i in 1..=GRID_X {
         for j in 1..=GRID_Y {
-            let mut adj = 0;
-            if grid_expanded[i - 1][j] == Life::Bug {
-                adj += 1;
+            grid[i - 1][j - 1] = next_state(
+                grid_expanded[i][j],
+                &[
+                    grid_expanded[i - 1][j],
+                    grid_expanded[i + 1][j],
+                    grid_expanded[i][j - 1],
+                    grid_expanded[i][j + 1],
+                ],
+            );
+        }
+    }
+}
+
+fn next_state(current_state: Life, adjacents: &[Life]) -> Life {
+    let s = adjacents
+        .iter()
+        .map(|a| if *a == Life::Bug { 1 } else { 0 })
+        .sum::<u32>();
+    match current_state {
+        Life::Bug => {
+            if s == 1 {
+                Life::Bug
+            } else {
+                Life::Empty
             }
-            if grid_expanded[i + 1][j] == Life::Bug {
-                adj += 1;
+        }
+        Life::Empty => {
+            if s == 1 || s == 2 {
+                Life::Bug
+            } else {
+                Life::Empty
             }
-            if grid_expanded[i][j - 1] == Life::Bug {
-                adj += 1;
-            }
-            if grid_expanded[i][j + 1] == Life::Bug {
-                adj += 1;
-            }
-            grid[i - 1][j - 1] = match grid_expanded[i][j] {
-                Life::Bug => {
-                    if adj == 1 {
-                        Life::Bug
-                    } else {
-                        Life::Empty
+        }
+    }
+}
+
+fn new_generation_recursive(grids: &mut VecDeque<GridLevel>) {
+    let outer_grid = [[Life::Empty; GRID_X]; GRID_Y];
+    let inner_grid = [[Life::Empty; GRID_X]; GRID_Y];
+    grids.push_back(outer_grid.clone());
+    grids.push_front(inner_grid.clone());
+    let gc = grids.clone();
+    for g in 1..grids.len() - 1 {
+        //println!("g {}", g);
+        // skip first grid since we know it's empty and it makes the algorithm work up in all cases
+        for i in 0..GRID_X {
+            for j in 0..GRID_Y {
+                //println!("({},{})", i, j);
+                match (i, j) {
+                    (0, 0) => {
+                        grids[g][i][j] = next_state(
+                            gc[g][i][j],
+                            &[
+                                gc[g - 1][GRID_X / 2][GRID_Y / 2 - 1],
+                                gc[g][i][j + 1],
+                                gc[g][i + 1][j],
+                                gc[g - 1][GRID_X / 2 - 1][GRID_Y / 2],
+                            ],
+                        )
                     }
-                }
-                Life::Empty => {
-                    if adj == 1 || adj == 2 {
-                        Life::Bug
-                    } else {
-                        Life::Empty
+                    (1, 0) | (2, 0) | (3, 0) => {
+                        // TODO: generalize arms like this for GRID_X and GRID_Y
+                        grids[g][i][j] = next_state(
+                            gc[g][i][j],
+                            &[
+                                gc[g - 1][GRID_X / 2][GRID_Y / 2 - 1],
+                                gc[g][i][j + 1],
+                                gc[g][i + 1][j],
+                                gc[g][i - 1][j],
+                            ],
+                        )
+                    }
+                    (4, 0) => {
+                        grids[g][i][j] = next_state(
+                            gc[g][i][j],
+                            &[
+                                gc[g - 1][GRID_X / 2][GRID_Y / 2 - 1],
+                                gc[g - 1][GRID_X / 2 + 1][GRID_Y / 2],
+                                gc[g][i - 1][j],
+                                gc[g][i][j + 1],
+                            ],
+                        )
+                    }
+                    (0, 4) => {
+                        grids[g][i][j] = next_state(
+                            gc[g][i][j],
+                            &[
+                                gc[g][i][j - 1],
+                                gc[g - 1][GRID_X / 2][GRID_Y / 2 + 1],
+                                gc[g][i + 1][j],
+                                gc[g - 1][GRID_X / 2 - 1][GRID_Y / 2],
+                            ],
+                        )
+                    }
+                    (0, 1) | (0, 2) | (0, 3) => {
+                        grids[g][i][j] = next_state(
+                            gc[g][i][j],
+                            &[
+                                gc[g][i][j - 1],
+                                gc[g][i][j + 1],
+                                gc[g][i + 1][j],
+                                gc[g - 1][GRID_X / 2 - 1][GRID_Y / 2],
+                            ],
+                        )
+                    }
+                    (1, 4) | (2, 4) | (3, 4) => {
+                        // TODO: generalize arms like this for GRID_X and GRID_Y
+                        grids[g][i][j] = next_state(
+                            gc[g][i][j],
+                            &[
+                                gc[g][i][j - 1],
+                                gc[g - 1][GRID_X / 2][GRID_Y / 2 + 1],
+                                gc[g][i + 1][j],
+                                gc[g][i - 1][j],
+                            ],
+                        )
+                    }
+                    (4, 1) | (4, 2) | (4, 3) => {
+                        // TODO: generalize arms like this for GRID_X and GRID_Y
+                        grids[g][i][j] = next_state(
+                            gc[g][i][j],
+                            &[
+                                gc[g][i][j - 1],
+                                gc[g][i][j + 1],
+                                gc[g - 1][GRID_X / 2 + 1][GRID_Y / 2],
+                                gc[g][i - 1][j],
+                            ],
+                        )
+                    }
+                    (4, 4) => {
+                        grids[g][i][j] = next_state(
+                            gc[g][i][j],
+                            &[
+                                gc[g][i][j - 1],
+                                gc[g - 1][GRID_X / 2][GRID_Y / 2 + 1],
+                                gc[g - 1][GRID_X / 2 + 1][GRID_Y / 2],
+                                gc[g][i - 1][j],
+                            ],
+                        )
+                    }
+                    (2, 2) => {}
+                    (2, 1) => {
+                        grids[g][i][j] = next_state(
+                            gc[g][i][j],
+                            &[
+                                gc[g][i][j - 1],
+                                gc[g][i - 1][j],
+                                gc[g][i + 1][j],
+                                gc[g + 1][0][0],
+                                gc[g + 1][1][0],
+                                gc[g + 1][2][0],
+                                gc[g + 1][3][0],
+                                gc[g + 1][4][0],
+                            ],
+                        )
+                    }
+                    (1, 2) => {
+                        grids[g][i][j] = next_state(
+                            gc[g][i][j],
+                            &[
+                                gc[g][i][j - 1],
+                                gc[g][i][j + 1],
+                                gc[g][i - 1][j],
+                                gc[g + 1][0][0],
+                                gc[g + 1][0][1],
+                                gc[g + 1][0][2],
+                                gc[g + 1][0][3],
+                                gc[g + 1][0][4],
+                            ],
+                        )
+                    }
+                    (3, 2) => {
+                        grids[g][i][j] = next_state(
+                            gc[g][i][j],
+                            &[
+                                gc[g][i][j - 1],
+                                gc[g][i][j + 1],
+                                gc[g][i + 1][j],
+                                gc[g + 1][4][0],
+                                gc[g + 1][4][1],
+                                gc[g + 1][4][2],
+                                gc[g + 1][4][3],
+                                gc[g + 1][4][4],
+                            ],
+                        )
+                    }
+                    (2, 3) => {
+                        grids[g][i][j] = next_state(
+                            gc[g][i][j],
+                            &[
+                                gc[g][i - 1][j],
+                                gc[g][i][j + 1],
+                                gc[g][i + 1][j],
+                                gc[g + 1][0][4],
+                                gc[g + 1][1][4],
+                                gc[g + 1][2][4],
+                                gc[g + 1][3][4],
+                                gc[g + 1][4][4],
+                            ],
+                        )
+                    }
+                    (1, 1) | (3, 1) | (1, 3) | (3, 3) => {
+                        grids[g][i][j] = next_state(
+                            gc[g][i][j],
+                            &[
+                                gc[g][i][j - 1],
+                                gc[g][i][j + 1],
+                                gc[g][i + 1][j],
+                                gc[g][i - 1][j],
+                            ],
+                        )
+                    }
+                    _ => {
+                        unreachable!();
                     }
                 }
             }
@@ -111,7 +300,6 @@ pub fn biodiversity_of_first_repeated_state(grid: &[[Life; GRID_X]; GRID_Y]) -> 
     let mut grid_mutable = *grid;
     let mut bio: HashSet<usize> = HashSet::new();
     while bio.insert(biodiversity(grid_mutable)) {
-        print_generation(grid_mutable);
         new_generation(&mut grid_mutable);
     }
     biodiversity(grid_mutable)
@@ -126,14 +314,10 @@ fn total_bugs_n_iterations(grid: &GridLevel, n: usize) -> usize {
     let mut grids: VecDeque<GridLevel> = VecDeque::new();
     grids.push_front(grid.clone());
     for _iterations in 0..n {
-        let outer_grid = [[Life::Empty; GRID_X]; GRID_Y];
-        let inner_grid = [[Life::Empty; GRID_X]; GRID_Y];
-        grids.push_back(outer_grid.clone());
-        grids.push_front(inner_grid.clone());
-        for g in grids.iter() {
-            if total_bugs(*g) == 0 {
-                continue;
-            }
+        println!("n{}", _iterations);
+        new_generation_recursive(&mut grids);
+        for g in grids.clone() {
+            print_generation(g);
         }
     }
     grids.iter().fold(0, |acc, g| acc + total_bugs(*g))
@@ -143,12 +327,19 @@ fn total_bugs_n_iterations(grid: &GridLevel, n: usize) -> usize {
 mod tests {
     use day24::biodiversity_of_first_repeated_state;
     use day24::generator;
-    use day24::solution_24b;
+    use day24::total_bugs_200_iterations;
+    use day24::total_bugs_n_iterations;
     use std::fs;
     const ANSWER_24A: usize = 1_151_290;
     const ANSWER_24B: usize = 0;
     const UNIT_ANSWER_24A: usize = 2_129_920;
+    const UNIT_ANSWER_24B: usize = 99;
     const UNIT_INPUT_24A: &str = r"....#
+#..#.
+#..##
+..#..
+#....";
+    const UNIT_INPUT_24B: &str = r"....#
 #..#.
 #..##
 ..#..
@@ -167,7 +358,7 @@ mod tests {
     fn t24b() {
         assert_eq!(
             ANSWER_24B,
-            solution_24b(&generator(
+            total_bugs_200_iterations(&generator(
                 &fs::read_to_string("input/2019/day24.txt").unwrap().trim()
             ))
         );
@@ -177,6 +368,13 @@ mod tests {
         assert_eq!(
             UNIT_ANSWER_24A,
             biodiversity_of_first_repeated_state(&generator(UNIT_INPUT_24A))
+        );
+    }
+    #[test]
+    fn t24b_supplied_inputs() {
+        assert_eq!(
+            UNIT_ANSWER_24B,
+            total_bugs_n_iterations(&generator(UNIT_INPUT_24B), 10)
         );
     }
 }
