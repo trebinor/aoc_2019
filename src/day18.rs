903b4904abb17bb85ea52728cc02ff8af8ecae08
trebinor <trebinor@users.noreply.github.com>
2020-01-12 15:25:04 -0500

    Day 18 part 1

--- src/day18.rs
+++ src/day18.rs
@@ -1,75 +1,91 @@
-//use std::collections::VecDeque;
+type Maze = Vec<Vec<char>>;
 
-pub struct Maze {
-    grid: Vec<Vec<char>>,
-    doors: Vec<char>,
-    origin: (usize, usize),
-}
-
-pub struct PathHead {
-    pub x: usize,
-    pub y: usize,
-    pub keys: Vec<char>,
-}
-
-impl Maze {
-    pub fn new(w: usize, h: usize) -> Maze {
-        Maze {
-            grid: vec![vec![' '; w]; h],
-            doors: Vec::new(),
-            origin: (0, 0),
-        }
-    }
-
-    #[allow(dead_code)]
-    fn display(&self, _maze: Maze) {
-        for r in 0..self.grid.len() {
-            for c in 0..self.grid[r].len() {
-                print!("{}", self.grid[r][c]);
-            }
-            println!();
-        }
-    }
-
-    /*
-    fn doors_locked(&self, paths: Vec<PathHead>) -> bool {
-        !paths.any(|p| p.keys().len() >= self.doors.len())
-    }
-    */
-}
-
-// TODO: File issue with gobanos/aoc-runner. Why can't I get Vec<Vec<char>> to work with the generator?
 #[allow(dead_code)]
+fn display(maze: Maze) {
+    for (j, y) in maze.iter().enumerate() {
+        for (i, _x) in y.iter().enumerate() {
+            print!("{}", maze[j][i]);
+        }
+        println!();
+    }
+}
+
 #[aoc_generator(day18)]
 pub fn generator(input: &str) -> Maze {
-    let v = input.lines().map(|l| l.trim()).collect::<Vec<&str>>();
-    let mut maze: Maze = Maze::new(v[0].len(), v.len());
-    for (i, l) in v.iter().enumerate() {
-        for (j, x) in l.chars().enumerate() {
-            maze.grid[i][j] = x;
-            match x {
-                '@' => maze.origin = (i, j),
-                'A'..='Z' => maze.doors.push(x),
-                _ => {}
-            }
+    let v = input.lines().map(|l| l).collect::<Vec<&str>>();
+    let mut maze: Maze = vec![vec![' '; v[0].len()]; v.len()];
+
+    for (y, l) in v.iter().enumerate() {
+        for (x, c) in l.chars().enumerate() {
+            maze[y][x] = c;
         }
     }
     maze
 }
 
-#[allow(dead_code)]
-#[aoc(day18, part1)]
-pub fn shortest_path_to_all_keys(_input: &Maze) -> usize {
-    //let maze = generator(input);
-    /*
-    let mut paths: VecDeque<PathHead> = VecDeque::new();
-    paths.push_back(origin);
-    while maze.doors_locked(paths) {
-        let heads = paths.len();
-        for p in paths {}
+#[derive(Clone, Debug, Eq, Hash, Ord, PartialEq, PartialOrd)]
+struct Point(i8, i8, u32);
+impl Point {
+    fn key_present(&self, key: char) -> bool {
+        self.2 & (1 << (key as u8 - 97u8)) != 0
     }
-    */
-    0
+
+    fn add_key(&mut self, key: char) {
+        match key {
+            'a'..='z' => self.2 |= 1 << (key as u8 - 97u8),
+            _ => unreachable!(),
+        }
+    }
+    fn successors(&self, input: &Maze) -> Vec<Point> {
+        let &Point(x, y, k) = self;
+        let mut path = Vec::new();
+        for m in [(0, 1), (1, 0), (0, -1), (-1, 0)].iter() {
+            let i: i8 = x + m.0;
+            let j: i8 = y + m.1;
+            let mut keys = k;
+            let mut key_point = Point(0, 0, k);
+            match input[j as usize][i as usize] {
+                '#' => continue,
+                door @ 'A'..='Z' => {
+                    if !self.key_present(door.to_ascii_lowercase()) {
+                        continue;
+                    }
+                }
+                key @ 'a'..='z' => {
+                    keys = {
+                        key_point.add_key(key);
+                        key_point.2
+                    }
+                }
+                _ => {}
+            }
+            path.push(Point(i, j, keys));
+        }
+        path
+    }
+}
+
+#[aoc(day18, part1)]
+pub fn shortest_path_to_all_keys(input: &Maze) -> usize {
+    let mut goal = Point(0, 0, 0);
+    let mut origin_x: i8 = 0;
+    let mut origin_y: i8 = 0;
+    for (j, y) in input.iter().enumerate() {
+        for (i, _x) in y.iter().enumerate() {
+            match input[j][i] {
+                '@' => {
+                    origin_x = i as i8;
+                    origin_y = j as i8;
+                }
+                key @ 'a'..='z' => goal.add_key(key),
+                _ => {}
+            }
+        }
+    }
+    let origin = Point(origin_x, origin_y, 0);
+    let shortest_path =
+        pathfinding::directed::bfs::bfs(&origin, |p| p.successors(input), |p| p.2 == goal.2);
+    shortest_path.unwrap().len() - 1 // off by 1 for some reason
 }
 
 #[aoc(day18, part2)]
@@ -77,13 +93,13 @@ pub fn solution_18b(_input: &Maze) -> usize {
     0
 }
 
-/*
 #[cfg(test)]
 mod tests {
+    use day18::generator;
     use day18::shortest_path_to_all_keys;
     use day18::solution_18b;
     use std::fs;
-    const ANSWER_18A: usize = 0;
+    const ANSWER_18A: usize = 4118;
     const ANSWER_18B: usize = 0;
     const UNIT_ANSWER_18A_1: usize = 8;
     const UNIT_ANSWER_18A_2: usize = 86;
@@ -123,50 +139,53 @@ mod tests {
     fn t18a() {
         assert_eq!(
             ANSWER_18A,
-            shortest_path_to_all_keys(&fs::read_to_string("input/2019/day18.txt").unwrap().trim())
+            shortest_path_to_all_keys(&generator(
+                &fs::read_to_string("input/2019/day18.txt").unwrap().trim()
+            ))
         );
     }
     #[test]
     fn t18b() {
         assert_eq!(
             ANSWER_18B,
-            solution_18b(&fs::read_to_string("input/2019/day18.txt").unwrap().trim())
+            solution_18b(&generator(
+                &fs::read_to_string("input/2019/day18.txt").unwrap().trim()
+            ))
         );
     }
     #[test]
     fn t18a_supplied_inputs_1() {
         assert_eq!(
             UNIT_ANSWER_18A_1,
-            shortest_path_to_all_keys(UNIT_INPUT_18A_1)
+            shortest_path_to_all_keys(&generator(UNIT_INPUT_18A_1))
         );
     }
     #[test]
     fn t18a_supplied_inputs_2() {
         assert_eq!(
             UNIT_ANSWER_18A_2,
-            shortest_path_to_all_keys(UNIT_INPUT_18A_2)
+            shortest_path_to_all_keys(&generator(UNIT_INPUT_18A_2))
         );
     }
     #[test]
     fn t18a_supplied_inputs_3() {
         assert_eq!(
             UNIT_ANSWER_18A_3,
-            shortest_path_to_all_keys(UNIT_INPUT_18A_3)
+            shortest_path_to_all_keys(&generator(UNIT_INPUT_18A_3))
         );
     }
     #[test]
     fn t18a_supplied_inputs_4() {
         assert_eq!(
             UNIT_ANSWER_18A_4,
-            shortest_path_to_all_keys(UNIT_INPUT_18A_4)
+            shortest_path_to_all_keys(&generator(UNIT_INPUT_18A_4))
         );
     }
     #[test]
     fn t18a_supplied_inputs_5() {
         assert_eq!(
             UNIT_ANSWER_18A_5,
-            shortest_path_to_all_keys(UNIT_INPUT_18A_5)
+            shortest_path_to_all_keys(&generator(UNIT_INPUT_18A_5))
         );
     }
 }
-*/
